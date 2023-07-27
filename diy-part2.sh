531668a68d2860d323f6f02d3a5dbe9101941999
llrboy <139751767+llrboy@users.noreply.github.com>
2023-07-27 21:19:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,6 +20,6 @@ find ./ -name luci-app-argon-config | xargs rm -rf;
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
-rm -rf feeds/small8/luci-app-adguardhome
+# rm -rf feeds/small8/luci-app-adguardhome
 
-git clone https://github.com/llrboy/luci-app-adguardhome.git package/net/luci-app-adguardhome
+# git clone https://github.com/llrboy/luci-app-adguardhome.git package/net/luci-app-adguardhome
